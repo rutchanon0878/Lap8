dab00258cbc041cfd2c70f7ca36b148ec5ed867b
rutchanon0878 <rutchanon_inkam@cmu.ac.th>
2023-12-12 16:46:48 +0700

    finish

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,42 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    int h;
+    int b;
+    cout << "Enter your age: ";
+    cin >> age;
+    
+    if (age <= 25){
+        cout << "Enter your height: ";
+        cin >> h;
+        if (h < 100){
+            cout << "Your character = Chopper"<< "\n";
+        }else if (h < 180){
+            cout << "Your character = Usopp"<< "\n";
+        }else{ 
+            cout << "Enter your bounty: ";
+            cin >> b;
+            if (b > 1100000000){
+              cout << "Your character = Zoro"<< "\n";
+        }else
+              cout << "Your character = Sanji"<< "\n";
+    }
+    }else {
+        if(age <= 60){
+            cout << "Enter your bounty: ";
+            cin >> b;
+            if (b > 500000000){
+            cout << "Your character = Jinbe"<< "\n";
+            } else {
+            cout << "Your character = Franky"<< "\n";
+            }
+        }else 
+            cout << "Your character = Brook"<< "\n";
+
+    }
+
+    return 0;
+}
+    
